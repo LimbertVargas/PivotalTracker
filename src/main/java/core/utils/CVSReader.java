ca7bb1daefeeafaa48959b0a5cde69b2c2f7970b
JohnSP <johnpiterzon@gmail.com>
2019-10-06 19:49:07 -0400

    CSV reader update

--- src/main/java/core/utils/CVSReader.java
+++ src/main/java/core/utils/CVSReader.java
@@ -7,25 +7,26 @@ import java.io.IOException;
 
 public class CVSReader {
 
-    public static void main(String[] args) {
+    public String[] getIdsStory() {
         String csvFile = System.getProperty("user.dir") + "/" + "src/test/resources/files/workshop2_20191006_1729.csv";
         BufferedReader br = null;
         String line = "";
         String cvsSplitBy = ",";
-        String ids[] = new String[22];
+        String ids[] = new String[23];
         try {
+            int j =0;
             br = new BufferedReader(new FileReader(csvFile));
             while ((line = br.readLine()) != null) {
                 String[] country = line.split(cvsSplitBy);
-                int j = 0;
-                j++;
                 ids[j] = country[0];
                 System.out.println(ids[j]);
+                j++;
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return ids;
     }
 }
